3d131407bfc2756a7ef5fa83cb10f341341ba108
Jonathan Bernardi <jon@jonbernardi.com>
2023-10-20 22:04:23 -0700

    add google analytics script

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,6 +6,7 @@ module.exports = {
     locale: 'en-US',
     version: String(Date.now()),
     gtm_tag_id: process.env.GTM_TAG_ID,
+    google_analytics: process.env.GOOGLE_ANALYTICS,
     disqus_short_name: 'spekkionu',
     twitter: '@thejonbernardi',
 }
