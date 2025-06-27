a824995924ac92d536a97677531f411922524003
Jonathan Bernardi <jon@jonbernardi.com>
2025-06-27 11:00:44 -0700

    add pageview trigger for spa

--- src/assets/js/transitions.js
+++ src/assets/js/transitions.js
@@ -1,4 +1,7 @@
 ;document.addEventListener('astro:page-load', function () {
+  if (window.zaraz && window.zaraz !== undefined) {
+    window.zaraz.spaPageview();
+  }
   if (document.getElementById('disqus_thread')) {
     lazyLoadDisqus();
   }
